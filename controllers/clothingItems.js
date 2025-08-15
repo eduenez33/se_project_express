45a638c648ee54d9ea7d02d480f13ac8ad331f55
Edwin Duenez <duenezedwin33@gmail.com>
2025-08-15 17:13:03 -0600

    handle errors

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,30 +1,68 @@
 const ClothingItem = require("../models/clothingItem");
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} = require("../utils/errors");
 
 const getClothingItems = (req, res) => {
   ClothingItem.find({})
     .then((items) => res.send(items))
-    .catch((err) => res.status(500).send({ message: "Error fetching items" }));
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error occurred on the server" });
+    });
 };
 
 const createClothingItem = (req, res) => {
   const { name, weather, imageUrl } = req.body;
 
-  console.log(req.user._id);
+  if (!name || !weather || !imageUrl) {
+    return res.status(BAD_REQUEST).send({
+      message: "Name, weather, and imageUrl are required",
+    });
+  }
 
-  ClothingItem.create({ name, weather, imageUrl })
+  const owner = req.user._id;
+
+  ClothingItem.create({ name, weather, imageUrl, owner })
     .then((item) => res.status(201).send(item))
-    .catch((err) => res.status(400).send({ message: "Error creating item" }));
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        res.status(BAD_REQUEST).send({ message: "Invalid data provided" });
+      } else {
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error occurred on the server" });
+      }
+    });
 };
 
 const deleteClothingItem = (req, res) => {
-  ClothingItem.findByIdAndRemove(req.params.itemId)
+  ClothingItem.findByIdAndDelete(req.params.itemId)
+    .orFail(() => {
+      const error = new Error("Item not found");
+      error.statusCode = NOT_FOUND;
+      return error;
+    })
     .then((item) => {
-      if (!item) {
-        return res.status(404).send({ message: "Item not found" });
-      }
       res.send({ message: "Item deleted successfully" });
     })
-    .catch((err) => res.status(500).send({ message: "Error deleting item" }));
+    .catch((err) => {
+      console.error(err);
+      if (err.statusCode === NOT_FOUND) {
+        res.status(NOT_FOUND).send({ message: err.message });
+      } else if (err.name === "CastError" || err.name === "ValidationError") {
+        res.status(BAD_REQUEST).send({ message: "Invalid item ID" });
+      } else {
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error occurred on the server" });
+      }
+    });
 };
 
 module.exports = { getClothingItems, createClothingItem, deleteClothingItem };
--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,66 @@
 const User = require("../models/user");
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} = require("../utils/errors");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch((err) => res.status(500).send({ message: "Error fetching users" }));
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error occurred on the server" });
+    });
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
+    .orFail(() => {
+      const error = new Error("User not found");
+      error.statusCode = NOT_FOUND;
+      return error;
+    })
     .then((user) => {
-      if (!user) {
-        return res.status(404).send({ message: "User not found" });
-      }
       res.send(user);
     })
-    .catch((err) => res.status(500).send({ message: "Error fetching user" }));
+    .catch((err) => {
+      console.error(err);
+      if (err.statusCode === NOT_FOUND) {
+        res.status(NOT_FOUND).send({ message: err.message });
+      } else if (err.name === "CastError" || err.name === "ValidationError") {
+        res.status(BAD_REQUEST).send({ message: "Invalid user ID" });
+      } else {
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error occurred on the server" });
+      }
+    });
 };
 
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
+  if (!name || !avatar) {
+    return res
+      .status(BAD_REQUEST)
+      .send({ message: "Name and avatar are required" });
+  }
+
   User.create({ name, avatar })
     .then((user) => res.status(201).send(user))
-    .catch((err) => res.status(400).send({ message: "Error creating user" }));
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        res.status(BAD_REQUEST).send({ message: "Invalid data provided" });
+      } else {
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error occurred on the server" });
+      }
+    });
 };
 
 module.exports = { getUsers, getUserById, createUser };
