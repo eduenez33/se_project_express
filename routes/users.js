ccde61e5244e9eddd6dfbd2497b26db9dba216c1
eduenez33 <duenezedwin33@gmail.com>
2025-08-24 11:00:13 -0600

    remove uneeded routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,3 @@
 const router = require("express").Router();
-const { getUsers, getUserById, createUser } = require("../controllers/users");
-
-router.get("/", getUsers);
-router.get("/:userId", getUserById);
-router.post("/", createUser);
 
 module.exports = router;
