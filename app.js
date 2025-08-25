222f5e4ada7a945e5143759f881a11e64e839be5
eduenez33 <duenezedwin33@gmail.com>
2025-08-25 11:15:04 -0600

    remove hardcoded user object

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "689fb0f58c28ac0e69297fa3",
-  };
-  next();
-});
-
 app.use(express.json());
 app.use("/", routes);
 
