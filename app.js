e42782587a9d632418b6ba6511883e14f44ab95e
eduenez33 <duenezedwin33@gmail.com>
2025-08-25 22:03:37 -0600

    install cors, include in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
@@ -8,6 +9,7 @@ const { PORT = 3001 } = process.env;
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
+app.use(cors());
 app.use("/", routes);
 
 app.listen(PORT, () => {
