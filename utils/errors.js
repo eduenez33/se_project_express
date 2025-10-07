d79ea1eece49cb1d3ab99e95fad9c362d038b2a6
Edwin Duenez <duenezedwin33@gmail.com>
2025-10-07 10:31:54 -0600

    add custom error classes for improved error handling

--- utils/errors.js
+++ utils/errors.js
@@ -7,4 +7,46 @@ const ERROR_CODES = {
   INTERNAL_SERVER_ERROR: 500,
 };
 
-module.exports = ERROR_CODES;
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+  }
+}
+
+class ForbiddenError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 403;
+  }
+}
+
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+  }
+}
+
+class ConflictError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 409;
+  }
+}
+
+module.exports = {
+  ...ERROR_CODES,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+};
