1b139c702d5d0b6a24422227af4138c25aab3b19
Edwin Duenez <duenezedwin33@gmail.com>
2025-10-07 12:19:34 -0600

    add celebrate error handling middleware to the application

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 const errorHandler = require("./middlewares/error-handler");
+const { errors } = require("celebrate");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -12,7 +13,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
-
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
