415342f3c83d2151f6e4e215960018aed00f0d5e
Edwin Duenez <duenezedwin33@gmail.com>
2025-10-07 11:32:08 -0600

    add validation middleware for clothing items and user information

---
+++ middlewares/validation.js
@@ -0,0 +1,39 @@
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+module.exports.validateClothingItemBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    weather: Joi.string().required().valid("hot", "warm", "cold"),
+    imageUrl: Joi.string().required().custom(validateURL),
+  }),
+});
+
+module.exports.validateUserInfoBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string().required().custom(validateURL),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports.validateUserLogIn = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports.validateClothingItemId = celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().hex().length(24).required(),
+  }),
+});
