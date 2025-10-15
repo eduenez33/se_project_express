27f38b3a1558b75b8db17db5a6f850e6b32749b1
Edwin Duenez <duenezedwin33@gmail.com>
2025-10-15 11:50:11 -0600

    modularize error classes and consolidate exports

---
+++ utils/errors/index.js
@@ -0,0 +1,23 @@
+const BadRequestError = require("./BadRequestError");
+const UnauthorizedError = require("./UnauthorizedError");
+const ForbiddenError = require("./ForbiddenError");
+const NotFoundError = require("./NotFoundError");
+const ConflictError = require("./ConflictError");
+
+const ERROR_CODES = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = {
+  ...ERROR_CODES,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+};
