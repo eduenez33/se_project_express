b5007156c1a4cb9c2fd4489cbce20b96bc61fe9a
Edwin Duenez <duenezedwin33@gmail.com>
2025-10-15 11:58:33 -0600

    add validation middleware for signup and signin routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
+const {
+  validateUserInfoBody,
+  validateUserLogIn,
+} = require("../middlewares/validation");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
-router.post("/signup", createUser);
-router.post("/signin", login);
+router.post("/signup", validateUserInfoBody, createUser);
+router.post("/signin", validateUserLogIn, login);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
