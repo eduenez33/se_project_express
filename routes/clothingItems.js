0fcde557b4639bd565874e7dfed1d1436a8471b8
Edwin Duenez <duenezedwin33@gmail.com>
2025-10-15 11:58:44 -0600

    add validation middleware to clothing items routes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -7,11 +7,15 @@ const {
   removeLike,
 } = require("../controllers/clothingItems");
 const auth = require("../middlewares/auth");
+const {
+  validateClothingItemBody,
+  validateId,
+} = require("../middlewares/validation");
 
 router.get("/", getClothingItems);
-router.post("/", auth, createClothingItem);
-router.delete("/:itemId", auth, deleteClothingItem);
-router.put("/:itemId/likes", auth, addLike);
-router.delete("/:itemId/likes", auth, removeLike);
+router.post("/", auth, validateClothingItemBody, createClothingItem);
+router.delete("/:itemId", auth, validateId, deleteClothingItem);
+router.put("/:itemId/likes", auth, validateId, addLike);
+router.delete("/:itemId/likes", auth, validateId, removeLike);
 
 module.exports = router;
