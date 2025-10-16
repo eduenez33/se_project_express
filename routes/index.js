5125bce4e9f1ee570db4b515ca521df5c593bbcd
Edwin Duenez <duenezedwin33@gmail.com>
2025-10-16 10:26:01 -0600

    refactor error handling for not found routes to use NotFoundError class

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
-const { NOT_FOUND } = require("../utils/errors");
+const NotFoundError = require("../utils/errors/NotFoundError");
 const {
   validateUserInfoBody,
   validateUserLogIn,
@@ -13,8 +13,8 @@ router.use("/items", clothingItemRouter);
 router.post("/signup", validateUserInfoBody, createUser);
 router.post("/signin", validateUserLogIn, login);
 
-router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
+router.use((req, res, next) => {
+  next(new NotFoundError("Requested resource not found"));
 });
 
 module.exports = router;
